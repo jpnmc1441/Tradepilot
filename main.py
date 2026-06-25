73c5bb2d71ce7acc085e33cdf6e67ce8d4d09dd8
jpnmc1441 <43360036+jpnmc1441@users.noreply.github.com>
2026-06-25 12:42:20 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
 import sys
+
 from PySide6.QtWidgets import QApplication
+
 from gui.main_window import MainWindow
-app=QApplication(sys.argv);w=MainWindow();w.show();sys.exit(app.exec())
+from core.application import Application
+
+
+def main():
+
+    application = Application()
+
+    application.start()
+
+    qt = QApplication(sys.argv)
+
+    window = MainWindow()
+
+    window.show()
+
+    sys.exit(qt.exec())
+
+
+if __name__ == "__main__":
+    main()
